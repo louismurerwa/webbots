7f2da233984532aab0c2f6e39af7c64b02d445c3
Louis Murerwa <35416595+LUXATHEMASTER@users.noreply.github.com>
2019-02-03 03:09:26 -0500

    removed keys

--- twiterbot.py
+++ twiterbot.py
@@ -5,10 +5,7 @@ import time
 import os
 
 #credidentials
-access_token ='992516614087159808-PwwohArdfhcW8NJgzUGtA7PtIZBQzwa'
-access_secret =	'FLGFeoAGyh22GMz9jAYgvC2JjLCVPPhajLiqAWyb270yC'
-consumer_key = 'wHmzwq9nEhgVfYexKkiPkMfOl'
-consumer_secret = 'Q1Rqe6kUx2KGYfWmmvg6vJVWUNX0ySWkZxEcWJlJvGZ0XMzPk5'
+
 
 # log in to twite
 
